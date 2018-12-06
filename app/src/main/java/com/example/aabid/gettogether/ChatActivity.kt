8eca2a6df97da3c44e3e8e03797c47f47ea3889a
Aabid Shamji <aabidshamji@gmail.com>
2018-12-06 17:21:28 +0100

    imported new modules for chat

---
+++ app/src/main/java/com/example/aabid/gettogether/ChatActivity.kt
@@ -0,0 +1,39 @@
+package com.example.aabid.gettogether
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import co.chatsdk.firebase.file_storage.FirebaseFileStorageModule
+import co.chatsdk.core.error.ChatSDKException
+import co.chatsdk.firebase.FirebaseNetworkAdapter
+import co.chatsdk.ui.manager.BaseInterfaceAdapter
+import co.chatsdk.core.session.ChatSDK
+import co.chatsdk.core.session.Configuration
+
+
+class ChatActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat)
+
+        val context = applicationContext
+        // Create a new configuration
+        val builder = Configuration.Builder(context)
+
+        // Perform any configuration steps (optional)
+        builder.firebaseRootPath("prod")
+
+        // Initialize the Chat SDK
+        try {
+            ChatSDK.initialize(builder.build(), BaseInterfaceAdapter(context), FirebaseNetworkAdapter())
+        } catch (e: ChatSDKException) {
+        }
+
+
+        // File storage is needed for profile image upload and image messages
+        FirebaseFileStorageModule.activate()
+        FirebasePushModule.activate()
+
+    }
+}
