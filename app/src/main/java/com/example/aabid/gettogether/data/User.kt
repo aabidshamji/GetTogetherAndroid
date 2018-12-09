7b0f4112456c9eb3f76e2942bc6dbdccda537050
adambigelow <adam.bigelow936@gmail.com>
2018-12-09 15:39:40 +0100

    Removed things

--- app/src/main/java/com/example/aabid/gettogether/data/User.kt
+++
@@ -1,6 +0,0 @@
-package com.example.aabid.gettogether.data
-
-data class User(var uid: String = "",
-                var name: String = "",
-                var latitude: Int = 0,
-                var longitude: Int = 0)
--- app/src/main/java/com/example/aabid/gettogether/touch/TouchHelperAdapter.kt
+++
@@ -1,6 +0,0 @@
-package com.example.aabid.gettogether.touch
-
-interface TouchHelperAdapter {
-    fun onDismissed(position: Int)
-    fun onCityMoved(fromPosition: Int, toPosition: Int)
-}
