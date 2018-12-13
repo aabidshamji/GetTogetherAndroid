54f49ddb9116f91c104c31be78123f54be045f40
adambigelow <adam.bigelow936@gmail.com>
2018-12-13 16:30:45 +0100

    Changes including doubles

--- app/src/main/java/com/example/aabid/gittogether/data/User.kt
+++ app/src/main/java/com/example/aabid/gittogether/data/User.kt
@@ -2,7 +2,7 @@ package com.example.aabid.gittogether.data
 
 data class User(var uid: String = "",
                 var name: String = "",
-                var latitude: Int = 0,
-                var longitude: Int = 0,
+                var latitude: Double = 0.0,
+                var longitude: Double = 0.0,
                 var groups: MutableList<String> = mutableListOf()
 )
